514b80906fee09deb5c9f7c4a348035b875707c3
Douglas Rogers <edwardDouglasRogers@gmail.com>
2018-03-31 15:59:58 -0400

    first commit

---
+++ src/main/java/com/example/demoVinDan/DemoVinDanApplication.java
@@ -0,0 +1,12 @@
+package com.example.demoVinDan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoVinDanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoVinDanApplication.class, args);
+	}
+}
